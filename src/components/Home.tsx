958222f232a89f3300b8f9e449c93f059f839019
Haitam Makhfi <haitammakhfi7@gmail.com>
2024-12-30 22:46:25 +0100

    implimented sorting logic in Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,19 +2,56 @@ import Nav from "./Nav";
 import TitleSort from "./TitleSort";
 import TagSort from "./TagSort";
 import NotesList from "./NotesList";
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { notesContext } from "../App";
+type noteType = {
+  data: { data: any };
+  noteValue: string;
+  titleValue: string;
+  tagValue: string[];
+  id: number;
+};
 function Home() {
   const { notes } = useContext(notesContext);
-  // const [displayNotes, setDisplayNotes] = useState(notes);
+  const [displayNotes, setDisplayNotes] = useState<noteType[]>(notes);
   const [tagValue, setTagValue] = useState([]);
   const [titleValue, setTitleValue] = useState("");
+  useEffect(() => {
+    setDisplayNotes([]);
+    notes.forEach((note: noteType) => {
+      if (titleValue == "" && compareWithTagValue(note.tagValue) == null) {
+        setDisplayNotes((p) => [...p, note]);
+      } else if (
+        note.titleValue == titleValue &&
+        compareWithTagValue(note.tagValue) == null
+      ) {
+        setDisplayNotes((p) => [...p, note]);
+      } else if (titleValue == "" && compareWithTagValue(note.tagValue)) {
+        setDisplayNotes((p) => [...p, note]);
+      } else if (
+        note.titleValue == titleValue &&
+        compareWithTagValue(note.tagValue)
+      ) {
+        setDisplayNotes((p) => [...p, note]);
+      }
+      return null;
+    });
+  }, [titleValue, tagValue]);
+  function compareWithTagValue(array: string[]) {
+    let bool = null;
+    tagValue.forEach((el) => {
+      array.includes(el) ? (bool = true) : (bool = false);
+    });
+    return bool;
+  }
+  // console.log(compareWithTagValue(["work", "personnel", "chopping"]));
+
   return (
     <main>
       <Nav />
       <TitleSort setTitleValue={setTitleValue} titleValue={titleValue} />
       <TagSort setTagValue={setTagValue} tagValue={tagValue} />
-      <NotesList notes={notes} />
+      <NotesList notes={displayNotes} />
     </main>
   );
 }
