06b610bd4e81990e5bc00f4808d487335a57107c
Haitam Makhfi <haitammakhfi7@gmail.com>
2025-01-01 00:04:56 +0100

    added navigation to home in the nav's h1

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Tooltip from "./Tooltip";
+import { useNavigate } from "react-router-dom";
 export default function Nav() {
   const [tooltipactive, setTooltipActive] = useState(false);
+  const navigate = useNavigate();
   return (
     <nav>
-      <h1>Clipy</h1>
+      <h1 onClick={() => navigate("/")}>Clipy</h1>
       <div className="nav-btns">
         <div className="edit-btn-container">
           <div
